d1c5794a11bd0326e280881c5a1adcf49b8ec689
sowicm <github@sowicm.com>
2016-10-22 00:46:07 +0800

    first init

---
+++ sfz.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python
+# coding=utf-8
+
+import PIL
+from PIL import ImageFont, Image, ImageDraw
+import sys
+import os
+
+front_file = os.path.expanduser('~')+"/projects/idlimit/sfz_front.jpg"
+back_file = os.path.expanduser('~')+"/projects/idlimit/sfz_back.jpg"
+
+font = ImageFont.truetype('/Library/Fonts/Songti.ttc', 40)
+
+im = Image.open(front_file)
+draw = ImageDraw.Draw(im)
+draw.text((100,715), u"仅供"+sys.argv[1].decode('utf-8')+u"使用——", (255, 255, 255), font=font)
+
+im.save('~/upload_front.jpg')
+
+im = Image.open(back_file)
+draw = ImageDraw.Draw(im)
+draw.text((130,715), u"仅供"+sys.argv[1].decode('utf-8')+u"使用——", (255, 255, 255), font=font)
+
+im.save('~/upload_back.jpg')
+
